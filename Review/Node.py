2115476e60b90ccf14dac64ceef52afffc57aa11
carlosandfound <foobaralvar@gmail.com>
2019-08-19 22:31:22 -0700

    Adding TreeNode class

--- Review/Node.py
+++ Review/Node.py
@@ -11,10 +11,29 @@ class Node:
 
     def __str__(self):
         output = "Data: " + self.data + "\nNext: "
-        if self.next:
-            output += self.next.data
-        else:
-            output += "null"
+        output += self.next.data if self.next else "null"
+        return output
+
+class TreeNode:
+    def __init__(self, data="", left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def setData(self, new_data):
+        self.data = new_data
+
+    def setLeftNode(self, left_node):
+        self.left = left_node
+
+    def setRightNode(self, right_node):
+        self.right = right_node
+
+    def __str__(self):
+        output = "Current Node: " + self.data + "; Left Child: "
+        output += self.left.data if self.left else "null"
+        output += "; Right Child: "
+        output += self.right.data if self.right else "null"
         return output
 
 def main():
@@ -29,6 +48,8 @@ def main():
     n.data = "dds"
     print(n.data)
     print(n)
+    t = TreeNode("ddd")
+    print(t)
 
 if __name__ == '__main__':
     main()
